ni/gptp

commit 67445e6b300dc63cf90e6e33a6027551e8a506cc

Added interface for standard Linux system cross timestamp code

linux/src/linux_hal_generic.cpp
@@ -44,10 +44,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <linux/net_tstamp.h>
+#include <linux/ptp_clock.h>
 #include <syscall.h>
+#include <limits.h>
 
+#if 0
 #define TX_PHY_TIME 184
 #define RX_PHY_TIME 382
+#endif
+#define TX_PHY_TIME 8000
+#define RX_PHY_TIME 8000
 
 net_result LinuxNetworkInterface::nrecv
 ( LinkLayerAddress *addr, uint8_t *payload, size_t &length ) {
@@ -140,7 +146,6 @@ net_result LinuxNetworkInterface::nrecv
 				ts_system = ((struct timespec *) CMSG_DATA(cmsg)) + 1;
 				system = tsToTimestamp( ts_system );
 				ts_device = ts_system + 1; device = tsToTimestamp( ts_device );
-				gtimestamper->updateCrossStamp( &system, &device );
 				device = device - latency;
 				gtimestamper->pushRXTimestamp( &device );
 				break;    
@@ -218,7 +223,6 @@ bool LinuxTimestamperGeneric::Adjust( void *tmx ) {
 
 bool LinuxTimestamperGeneric::HWTimestamper_init
 ( InterfaceLabel *iface_label, OSNetworkInterface *iface ) {
-	int fd;
 	cross_stamp_good = false;
 	int phc_index;
 	char ptp_device[] = PTP_DEVICE;
@@ -229,7 +233,7 @@ bool LinuxTimestamperGeneric::HWTimestamper_init
 
 	// Determine the correct PTP clock interface
 	phc_index = findPhcIndex( iface_label );
-	if( phc_index < 0 ) {
+if( phc_index < 0 ) {
 		fprintf( stderr, "Failed to find PTP device index\n" );
 		return false;
 	}
@@ -238,8 +242,8 @@ bool LinuxTimestamperGeneric::HWTimestamper_init
 		( ptp_device+PTP_DEVICE_IDX_OFFS,
 		  sizeof(ptp_device)-PTP_DEVICE_IDX_OFFS, "%d", phc_index );
 	fprintf( stderr, "Using clock device: %s\n", ptp_device );
-	fd = open( ptp_device, O_RDWR );
-	if( fd == -1 || (_private->clockid = FD_TO_CLOCKID(fd)) == -1 ) {
+	phc_fd = open( ptp_device, O_RDWR );
+	if( phc_fd == -1 || (_private->clockid = FD_TO_CLOCKID(phc_fd)) == -1 ) {
 		fprintf( stderr, "Failed to open PTP clock device\n" );
 		return false;
 	}
@@ -312,7 +316,6 @@ int LinuxTimestamperGeneric::HWTimestamper_txtimestamp
 			system._version = version;
 			device = device + latency;
 			device._version = version;
-			updateCrossStamp( &system, &device );
 			timestamp = device;
 			ret = 0;
 			break;    
@@ -378,26 +381,70 @@ bool LinuxTimestamperGeneric::post_init( int ifindex, int sd, TicketingLock *loc
 	return true;
 }    
 
-void LinuxTimestamperGeneric::updateCrossStamp( Timestamp *system_time, Timestamp *device_time ) {
-	pthread_mutex_lock( &_private->cross_stamp_lock );
-	crstamp_system = *system_time;
-	crstamp_device  = *device_time;
-	cross_stamp_good = true;
-	pthread_mutex_unlock( &_private->cross_stamp_lock );
+#define MAX_NSEC 1000000000
+
+/* Return *a - *b */
+static inline ptp_clock_time pct_diff
+( struct ptp_clock_time *a, struct ptp_clock_time *b ) {
+	ptp_clock_time result;
+	if( a->nsec >= b->nsec ) {
+		result.nsec = a->nsec - b->nsec;
+	} else {
+		--a->sec;
+		result.nsec = (MAX_NSEC - b->nsec) + a->nsec;
+	}
+	result.sec = a->sec - b->sec;
+	
+	return result;
+}
+
+static inline int64_t pctns(struct ptp_clock_time t)
+{
+	return t.sec * 1000000000LL + t.nsec;
+}
+
+static inline Timestamp pctTimestamp( struct ptp_clock_time *t ) {
+	Timestamp result;
+
+	result.seconds_ls = t->sec & 0xFFFFFFFF;
+	result.seconds_ms = t->sec >> sizeof(result.seconds_ls)*8;
+	result.nanoseconds = t->nsec;
+
+	return result;
 }
 
 bool LinuxTimestamperGeneric::HWTimestamper_gettime
 ( Timestamp *system_time, Timestamp *device_time, uint32_t *local_clock,
   uint32_t *nominal_clock_rate ) {
-	bool ret = false;
-	pthread_mutex_lock( &_private->cross_stamp_lock );
-	if( cross_stamp_good ) {
-		*system_time = crstamp_system;
-		*device_time = crstamp_device;
-		ret = true;
+	unsigned i;
+	struct ptp_sys_offset offset;
+	struct ptp_clock_time *pct;
+	struct ptp_clock_time *system_time_l, *device_time_l;
+	
+	int64_t interval = LLONG_MAX;
+
+	if( phc_fd != -1 ) {
+		memset( &offset, 0, sizeof(offset));
+		offset.n_samples = PTP_MAX_SAMPLES;
+		ioctl( phc_fd, PTP_SYS_OFFSET, &offset );
+
+		pct = &offset.ts[0];
+		for( i = 0; i < offset.n_samples; ++i ) {
+			int64_t interval_t;
+			interval_t = pctns(pct_diff( pct+2*i+2, pct+2*i ));
+			if( interval_t < interval ) {
+				system_time_l = pct+2*i;
+				device_time_l = pct+2*i+1;
+				interval = interval_t;
+			}
+		}
+
+		*device_time = pctTimestamp( device_time_l );
+		*system_time = pctTimestamp( system_time_l );
+
+		return true;
 	}
-	pthread_mutex_unlock( &_private->cross_stamp_lock );
-		
-	return ret;
+
+	return false;
 }
 

common/ptp_message.cpp
@@ -1423,6 +1423,8 @@ void PTPMessagePathDelayRespFollowUp::processMessage(IEEE1588Port * port)
 	link_delay -= turn_around;
 	link_delay /= 2;
 
+	fprintf( stderr, "Link Delay = %ld\n", link_delay );
+
 	{
 		uint64_t mine_elapsed;
 	    uint64_t theirs_elapsed;
@@ -1433,7 +1435,7 @@ void PTPMessagePathDelayRespFollowUp::processMessage(IEEE1588Port * port)
 			mine_elapsed =  TIMESTAMP_TO_NS(request_tx_timestamp)-TIMESTAMP_TO_NS(prev_peer_ts_mine);
 			theirs_elapsed = TIMESTAMP_TO_NS(remote_req_rx_timestamp)-TIMESTAMP_TO_NS(prev_peer_ts_theirs);
 			theirs_elapsed -= port->getLinkDelay();
-			theirs_elapsed += link_delay;
+			theirs_elapsed += link_delay < 0 ? 0 : link_delay;
 			rate_offset =  ((FrequencyRatio) mine_elapsed)/theirs_elapsed;
 			port->setPeerRateOffset(rate_offset);
 			port->setAsCapable( true );

linux/src/linux_hal_generic.hpp
@@ -47,6 +47,7 @@ typedef struct LinuxTimestamperIGBPrivate * LinuxTimestamperIGBPrivate_t;
 class LinuxTimestamperGeneric : public LinuxTimestamper {
 private:
 	int sd;
+	int phc_fd;
 	Timestamp crstamp_system;
 	Timestamp crstamp_device;
 	LinuxTimestamperGenericPrivate_t _private;
@@ -66,8 +67,6 @@ public:
 	bool Adjust( void *tmx );
 	virtual bool HWTimestamper_init
 	( InterfaceLabel *iface_label, OSNetworkInterface *iface );
-
-	void updateCrossStamp( Timestamp *system_time, Timestamp *device_time );
 
 	void pushRXTimestamp( Timestamp *tstamp ) {
 		tstamp->_version = version;

linux/src/platform.hpp
@@ -36,8 +36,8 @@
 
 #include <stdint.h>
 
-#define PLAT_strncpy( dest, src, max ) strncpy( dest, src, max+1 );
-#define PLAT_snprintf(...) snprintf( __VA_ARGS__ );
+#define PLAT_strncpy( dest, src, max ) strncpy( dest, src, max+1 )
+#define PLAT_snprintf(...) snprintf( __VA_ARGS__ )
 
 uint16_t PLAT_htons( uint16_t s );
 uint32_t PLAT_htonl( uint32_t l );

commit e5a0116bcc0bcd6b7c9053bb529e83d7ee1c8aaf

Fix bogus PHY latency values in gPTP Linux HAL
On 2014-12-18 commit 67445e6b300dc63cf90e6e33a6027551e8a506cc introduced
a problem for users of the generic Linux HAL--some extremely large PHY
latency values (probably to offset latency introduced by a net sniffer)
were accidentally (I assume) committed. These cause low link delay values
to become negative.

This commit removes the preprocessor directives that replaced the original
latency values with the large ones.

linux/src/linux_hal_generic.cpp
@@ -48,12 +48,8 @@
 #include <syscall.h>
 #include <limits.h>
 
-#if 0
 #define TX_PHY_TIME 184
 #define RX_PHY_TIME 382
-#endif
-#define TX_PHY_TIME 8000
-#define RX_PHY_TIME 8000
 
 net_result LinuxNetworkInterface::nrecv
 ( LinkLayerAddress *addr, uint8_t *payload, size_t &length ) {