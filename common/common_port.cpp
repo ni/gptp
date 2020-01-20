f3c81f44263d31714efc8495a51482fbfb393f7b
Vineeth Acharya <vineeth.acharya@ni.com>
2020-01-20 17:22:40 -0600

    fix whitespaces and indenting

--- common/common_port.cpp
+++ common/common_port.cpp
@@ -740,9 +740,9 @@ bool CommonPort::processEvent( Event e )
 			uint32_t local_clock, nominal_clock_rate;
 			FrequencyRatio local_system_freq_offset;
 			int64_t local_system_offset;
-         		uint64_t mean_path_delay;
+			uint64_t mean_path_delay;
 
-         		getLinkDelay
+			getLinkDelay
 				( &mean_path_delay );
 
 			getDeviceTime
@@ -821,8 +821,8 @@ void CommonPort::setAsCapable(bool ascap)
 		int64_t local_system_offset;
 		uint64_t mean_path_delay;
 
-      		getLinkDelay
-         		( &mean_path_delay );
+		getLinkDelay
+			( &mean_path_delay );
 
 		getDeviceTime
 			( system_time, device_time,
