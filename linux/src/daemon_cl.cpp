4ae5116549bdf47324720554f2381dbcf970ec99
Wai Chun Ang <wai.chun.ang@emerson.com>
2025-08-14 15:06:59 +0800

    Fix merge conflict error to remove unused variable

--- linux/src/daemon_cl.cpp
+++ linux/src/daemon_cl.cpp
@@ -151,8 +151,6 @@ int main(int argc, char **argv)
 	bool automotiveProfile = false;
 	bool pps = false;
 	uint8_t priority1 = 248;
-	bool override_portstate = false;
-	PortState port_state = PTP_SLAVE;
 	char *restoredata = NULL;
 	char *restoredataptr = NULL;
 	off_t restoredatalength = 0;
